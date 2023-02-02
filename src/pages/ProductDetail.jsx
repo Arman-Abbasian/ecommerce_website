00d3267720e0b55ff4af034997864910fadf7744
Arman Abbasian <105098083+Arman-Abbasian@users.noreply.github.com>
2023-02-02 12:03:18 +0330

    work on ProductDetail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -3,17 +3,27 @@ import { toast } from "react-hot-toast";
 import { useParams } from "react-router-dom";
 import { useCard, useCardActions } from "../Providers/CardProvider";
 import http from "../services/httpService";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
 
 const ProductDetail = () => {
   const card = useCard();
+  console.log(card);
   const selectedItemId = useParams().id;
-  const { addToCart } = useCardActions();
+  const { addToCart, initialLoading } = useCardActions();
   const [selectedItem, setSelectedItem] = useState({
     data: null,
     error: null,
     loading: false,
   });
-  const [cardItem, setCardItem] = useState(null);
   useEffect(() => {
     setSelectedItem({ data: null, error: null, loading: true });
     http
@@ -26,6 +36,9 @@ const ProductDetail = () => {
         toast.error(err.message);
       });
   }, []);
+  useEffect(() => {
+    initialLoading();
+  }, []);
 
   function findId(item) {
     return card.findIndex((element) => element.id.toString() === item);
@@ -34,7 +47,54 @@ const ProductDetail = () => {
   if (selectedItem.data && card) {
     return (
       <div className="flex flex-col">
-        <div className="flex-1">image</div>
+        {/* image section */}
+        <div className="sm:hidden">
+          <Swiper
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            pagination={true}
+            modules={[EffectCoverflow, Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div class="aspect-w-3 aspect-h-2">
+                <img
+                  src={selectedItem.data.image}
+                  alt={selectedItem.data.name}
+                  class="w-full h-full object-center object-contain"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div class="aspect-w-3 aspect-h-2">
+                <img
+                  src={selectedItem.data.image}
+                  alt={selectedItem.data.name}
+                  class="w-full h-full object-center object-contain"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div class="aspect-w-3 aspect-h-2">
+                <img
+                  src={selectedItem.data.image}
+                  alt={selectedItem.data.name}
+                  class="w-full h-full object-center object-contain"
+                />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        {/* product detail */}
         <div>
           <p>{selectedItem.data.name}</p>
           <p>{selectedItem.data.car}</p>
